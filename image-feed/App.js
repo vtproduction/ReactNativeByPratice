f86f7f53afc45d1760f1b9960a3d4dc40428314c
NIEN-TOMO <nienbkict@gmail.com>
2019-08-20 18:13:09 +0700

    update

--- image-feed/App.js
+++ image-feed/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
+import Constants from 'expo-constants';
+import Feed from './screens/Feed';
+
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Feed style={styles.feed}/>
     </View>
+    
   );
 }
 
+
+const platformVersion = Platform.OS === 'ios'
+  ? parseInt(Platform.Version, 10)
+  : Platform.Version;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  feed: {
+    flex: 1,
+    marginTop:
+      Platform.OS === 'android' || platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
   },
 });
